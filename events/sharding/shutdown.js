0622c21ee107fbc1be7fde5ff41feb676a1e3d9b
Music Maker <joshuanejdl@gmail.com>
2024-10-15 01:30:18 -0500

    Soft shutdown event, emitted from ShardManager and IPC protocol

---
+++ events/sharding/shutdown.js
@@ -0,0 +1,10 @@
+module.exports = {
+	// This is called if the manager ever requests the shard to terminate
+	// You have 10 seconds to finish whatever you're doing before the shard is forcefully killed
+	name: 'shutdown',
+	execute: async function (client) {
+		console.log(`Shard ${client.shard.ids[0]} is shutting down...`);
+		client.destroy();
+		process.exit(0);
+	}
+}
