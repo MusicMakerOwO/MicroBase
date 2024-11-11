afed64ac7c386084ed79dd2a1bed6eb0a6ac636f
Music Maker <joshuanejdl@gmail.com>
2024-11-10 20:54:26 -0600

    perf: Little refactor for CRC32 to avoid generating table on the fly

--- TS/Utils/crc32.ts
+++ TS/Utils/crc32.ts
@@ -2,16 +2,16 @@
 // You should never use this to store passwords or any sensitive data
 // This is only useful for checking if data has been modified or corrupted
 
-export default function CRC32(str: string) {
-	const table = new Uint32Array(256);
-	for (let i = 0; i < 256; i++) {
-		let c = i;
-		for (let k = 0; k < 8; k++) {
-			c = c & 1 ? 0xEDB88320 ^ c >>> 1 : c >>> 1;
-		}
-		table[i] = c;
+const table = new Uint32Array(256);
+for (let i = 0; i < 256; i++) {
+	let c = i;
+	for (let k = 0; k < 8; k++) {
+		c = c & 1 ? 0xEDB88320 ^ c >>> 1 : c >>> 1;
 	}
+	table[i] = c;
+}
 
+export default function CRC32(str: string) {
 	let crc = 0 ^ -1;
 	for (let i = 0; i < str.length; i++) {
 		crc = table[(crc ^ str.charCodeAt(i)) & 0xff] ^ crc >>> 8;
