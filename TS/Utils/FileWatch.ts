72e2878b6f12b27cda2fb673bd626b353321199a
Music Maker <joshuanejdl@gmail.com>
2024-11-11 00:02:45 -0600

    refactor: Converted a forgotten require()

--- TS/Utils/FileWatch.ts
+++ TS/Utils/FileWatch.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { MicroClient } from '../typings';
-const Debounce = require('./Debounce.js');
+import Debounce from './Debounce';
 
 const FOLDERS = [
 	'context',
