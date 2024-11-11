ac1184ad33e90f759896cbb7c00a09e2a5e348d2
Music Maker <joshuanejdl@gmail.com>
2024-11-10 20:53:04 -0600

    feat: Restart command to showcase shard restarting

---
+++ TS/commands/restart.ts
@@ -0,0 +1,14 @@
+// const { SlashCommandBuilder } = require('@discordjs/builders');
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction } from '../typings';
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('restart')
+		.setDescription('Restarts the bot.'),
+	async execute(interaction: CommandInteraction) {
+		interaction.reply('Restarting...').then(() => {
+			process.exit(1);
+		});
+	}
+}
