abe60536c09b33344d115fd9d593a8cc0df14f2f
Music Maker <joshuanejdl@gmail.com>
2024-11-12 11:18:07 -0600

    refactor: ProcessHandler no longer exports a function, runs on import

--- TS/Utils/ProcessHandler.ts
+++ TS/Utils/ProcessHandler.ts
@@ -1,40 +1,34 @@
 import Log from './Logs';
 
-export default function ProcessManager () {
+// Crtl + C
+process.on('SIGINT', () => {
+	console.log();
+	Log.error('SIGINT: Exiting...');
+	process.exit(0);
+});
 
-	// Crtl + C
-	process.on('SIGINT', () => {
-		console.log();
-		Log.error('SIGINT: Exiting...');
-		process.exit(0);
-	});
+// Standard crash
+process.on('uncaughtException', (err) => {
+	Log.error(`UNCAUGHT EXCEPTION: ${err}`);
+});
 
-	// Standard crash
-	process.on('uncaughtException', (err) => {
-		Log.error(`UNCAUGHT EXCEPTION: ${err}`);
-	});
+// Killed process
+process.on('SIGTERM', () => {
+	Log.error('SIGTERM: Exiting...');
+	process.exit(0);
+});
 
-	// Killed process
-	process.on('SIGTERM', () => {
-		Log.error('SIGTERM: Exiting...');
-		process.exit(0);
-	});
+// Standard crash
+process.on('unhandledRejection', (err) => {
+	Log.error(`UNHANDLED REJECTION: ${err}`);
+});
 
-	// Standard crash
-	process.on('unhandledRejection', (err) => {
-		Log.error(`UNHANDLED REJECTION: ${err}`);
-	});
+// Deprecation warnings
+process.on('warning', (warning) => {
+	Log.warn(`WARNING: ${warning.name} : ${warning.message}`);
+});
 
-	// Deprecation warnings
-	process.on('warning', (warning) => {
-		Log.warn(`WARNING: ${warning.name} : ${warning.message}`);
-	});
-
-	// Reference errors
-	process.on('uncaughtReferenceError', (err) => {
-		Log.error(err);
-	});
-
-};
-
-module.exports = exports.default;
+// Reference errors
+process.on('uncaughtReferenceError', (err) => {
+	Log.error(err);
+});
