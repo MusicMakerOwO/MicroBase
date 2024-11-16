52371551dcd914fa6b530ec1c8570644191cb7a6
Music Maker <joshuanejdl@gmail.com>
2024-11-16 14:51:23 -0600

    refactor: Renamed clientPerms to botPerms

--- TS/commands/restricted.ts
+++ TS/commands/restricted.ts
@@ -4,12 +4,14 @@ import { CommandInteraction } from '../typings';
 
 export default {
 	dev: true, // hide from all guilds except from config.json
+	owner: true, // server owner only
 	guilds: ['123456789012345678'], // guild whitelist via ID
+	channels: ['123456789012345678'], // channel whitelist via ID
 	roles: ['123456789012345678'], // required to have one of the listed role IDs
 	users: ['123456789012345678'], // user whitelist via ID
 	cooldown: 5, // cooldown in seconds
 	userPerms: ['ManageGuild'], // required user permissions
-	clientPerms: ['Administrator'], // required bot permissions
+	botPerms: ['Administrator'], // required bot permissions
 	data: new SlashCommandBuilder()
 		.setName('restricted')
 		.setDescription('A restricted command')
