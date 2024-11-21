93f5c95b622d930c4863f5f2e15bf323c46dff00
Music Maker <joshuanejdl@gmail.com>
2024-11-21 10:59:49 -0600

    feat/fix: ReadFolder checks the path is absolute, not sure if it's a feature or a fix lol

--- TS/Utils/ReadFolder.ts
+++ TS/Utils/ReadFolder.ts
@@ -9,6 +9,8 @@ interface File {
 const files: File[] = [];
 
 export default function (path: string, depth = 3) {
+	if (!path.startsWith('/')) throw new Error(`Path must be absolute - Received ${path}`);
+	if (path.endsWith('/')) path = path.slice(0, -1);
 	files.length = 0;
 	ReadFolder(path, depth);
 	return files;
