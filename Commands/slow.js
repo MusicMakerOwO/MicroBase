86e527f0b7b7f6a0d977f99b5bf25fbab2ab2600
Music Maker <joshuanejdl@gmail.com>
2025-05-10 13:38:42 -0500

    feat: Rebuild /slow command to showcase caching and defering

--- Commands/slow.js
+++ Commands/slow.js
@@ -1,22 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-	defer: false, // defer without being ephemeral
-	cache: true, // the next time you run this command it will jump straight to the bottom without delay
 	data: new SlashCommandBuilder()
 	.setName('slow')
 	.setDescription('Demo for caching in microbase'),
 	execute: async function (interaction) {
-
-		// Spin the CPU for 3 seconds, we are gauranteed to get a "application did not respond" error
-		const start = Date.now();
-		while (Date.now() - start < 3000) { }
-
-		await interaction.editReply('This is a slow command...');
 		
-		// Simulate lag
-		await new Promise(resolve => setTimeout(resolve, 3000));
+		await new Promise(resolve => setTimeout(resolve, 4000)); // stall for 4 seconds
 
-		await interaction.editReply('Finished!');
+		await interaction.reply('Finished!');
 	}
 };
