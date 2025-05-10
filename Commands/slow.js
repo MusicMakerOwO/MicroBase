4056dd0ade781c688b4d804c19efb591bbf319a8
Music Maker <joshuanejdl@gmail.com>
2025-05-10 13:38:50 -0500

    previous commit

--- Commands/slow.js
+++ Commands/slow.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
+	cache: true,
 	data: new SlashCommandBuilder()
 	.setName('slow')
-	.setDescription('Demo for caching in microbase'),
+	.setDescription('Demo for caching and auto-defer in microbase'),
 	execute: async function (interaction) {
 		
 		await new Promise(resolve => setTimeout(resolve, 4000)); // stall for 4 seconds
