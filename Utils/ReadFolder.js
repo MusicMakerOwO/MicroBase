dc78260d253a6618d3edd5294b24352660b3c620
Music Maker <joshuanejdl@gmail.com>
2025-05-10 13:17:08 -0500

    fix: Support for Windows drive checking

--- Utils/ReadFolder.js
+++ Utils/ReadFolder.js
@@ -3,8 +3,10 @@ const Log = require('./Logs');
 
 const files = []; // string[] of paths
 
+const WINDOWS_DRIVE = /^[A-Z]:\\$/i;
+
 module.exports = function (path, depth = 3) {
-	if (!path.startsWith('/') && !path.startsWith('C:\\')) throw new Error(`Path must be absolute - Received ${path}`);
+	if (!path.startsWith('/') && !WINDOWS_DRIVE.test(path)) throw new Error(`Path must be absolute - Received ${path}`);
 	if (path.endsWith('/')) path = path.slice(0, -1);
 	files.length = 0;
 	ReadFolder(path, depth);
