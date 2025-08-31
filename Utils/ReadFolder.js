fb7d34c7017f37b95805ee3d1a9ad64381b469fb
Loxt_Nixo <128501463+loxt-nixo@users.noreply.github.com>
2025-08-31 17:03:09 +0200

    update: better way to check if path is absolute

--- Utils/ReadFolder.js
+++ Utils/ReadFolder.js
@@ -1,12 +1,13 @@
 const fs = require('node:fs');
 const Log = require('./Logs');
+const nodePath = require('path');
 
 const files = []; // string[] of paths
 
-const WINDOWS_DRIVE = /^[A-Z]:\\$/i;
-
 module.exports = function (path, depth = 3) {
-	if (!path.startsWith('/') && !WINDOWS_DRIVE.test(path)) throw new Error(`Path must be absolute - Received ${path}`);
+	if (!nodePath.isAbsolute(path)) {
+		throw new Error(`Path must be absolute - Received ${path}`);
+	}
 	if (path.endsWith('/')) path = path.slice(0, -1);
 	files.length = 0;
 	ReadFolder(path, depth);
